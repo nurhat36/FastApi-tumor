83450e90b46acb3dc2fe324ece2b521af14abf26
Nurhat Baydağ <baydagnurhat31@gmail.com>
2026-01-16 17:28:55 +0300

    initial

--- app/models/unet_model.py
+++ app/models/unet_model.py
@@ -4,7 +4,7 @@ from app.models.advanced_unet import build_advanced_unet
 
 model = build_advanced_unet()
 
-model = load_model('app/static/tumor_segmentation_model.h5', custom_objects={
+model = load_model('app/static/best_model.h5', custom_objects={
     'dice_loss': dice_loss,
     'dice_coef': dice_coef,
     'iou_metric': iou_metric
